52271322b01283883c35ece1823173d39d24bf8c
heromo <hero@asterisk-works.jp>
2022-08-20 16:05:30 +0900

    chap-16, ss-060

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go_todo_app
 
 go 1.19
 
-require golang.org/x/sync v0.0.0-20220819030929-7fc1605a5dde // indirect
+require (
+	github.com/caarlos0/env/v6 v6.9.3 // indirect
+	golang.org/x/sync v0.0.0-20220819030929-7fc1605a5dde // indirect
+)
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"go_todo_app/config"
 	"log"
 	"net"
 	"net/http"
@@ -12,22 +13,24 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Printf("need port number\n")
-		os.Exit(1)
-	}
-	p := os.Args[1]
-	l, err := net.Listen("tcp", ":"+p)
-	if err != nil {
-		log.Fatalf("failed to listen port %s: %v", p, err)
-	}
-	if err := run(context.Background(), l); err != nil {
+	if err := run(context.Background()); err != nil {
 		log.Printf("failed to terminate server: %v", err)
 		os.Exit(1)
 	}
 }
 
-func run(ctx context.Context, l net.Listener) error {
+func run(ctx context.Context) error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+	}
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
+
 	s := &http.Server{
 		// hi引数で受け取った net.Listener を利用するので
 		// Addr フィールドは指定しない
