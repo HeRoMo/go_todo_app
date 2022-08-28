611d829ae2d931bc005612f6dc0b3cf26845cb84
heromo <hero@asterisk-works.jp>
2022-08-28 17:34:18 +0900

    fix schema

--- _tools/mysql/schema.sql
+++ _tools/mysql/schema.sql
@@ -5,6 +5,7 @@ CREATE TABLE `user`
   `password` VARCHAR(80) NOT NULL COMMENT 'パスワードハッシュ',
   `role` VARCHAR(80) NOT NULL COMMENT 'ロール',
   `created` DATETIME(6) NOT NULL COMMENT 'レコード作成日時',
+  `modified` DATETIME(6) NOT NULL COMMENT 'レコード修正日時',
   PRIMARY KEY (`id`),
   UNIQUE KEY `uix_name` (`name`) USING BTREE
 ) Engine=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='ユーザー';
